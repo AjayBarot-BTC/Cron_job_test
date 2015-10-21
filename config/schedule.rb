c1cbe5d5c8bef6186126f9d641952178ecb5e427
AjayBarot-BTC <ajay.barot@botreeconsulting.com>
2015-10-21 18:49:30 +0530

    run successfully cron job in local environment

--- config/schedule.rb
+++ config/schedule.rb
@@ -18,7 +18,8 @@
 # end
 
 # Learn more: http://github.com/javan/whenever
-set :environment, "production"
+env :GEM_PATH, ENV['GEM_PATH']
+set :environment, "development"
 set :output, {:error => "log/cron_error_log.log", :standard => "log/cron_log.log"}
 
 every 2.minutes do
