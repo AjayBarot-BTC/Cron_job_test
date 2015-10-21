ca10c67999df87f9775d24aa27727d0e4373632a
AjayBarot-BTC <ajay.barot@botreeconsulting.com>
2015-10-21 15:49:20 +0530

    schedule change

--- config/schedule.rb
+++ config/schedule.rb
@@ -21,6 +21,6 @@
 set :environment, "production"
 set :output, {:error => "log/cron_error_log.log", :standard => "log/cron_log.log"}
 
-every 5.minutes do
+every 2.minutes do
   rake "events:test"
 end
